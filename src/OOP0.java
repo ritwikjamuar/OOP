27243eb0fbf5e2dc3878741743bc714fdefd78da
Ritwik Jamuar <ritwik@recrosoft.com>
2017-03-25 23:59:40 +0530

    Basic Introduction with OOP

--- src/OOP0.java
+++ src/OOP0.java
@@ -1,4 +1,33 @@
-public class OOP0
-{
-	
-}
+/**
+ * Object Oriented Programming is a methodology or paradigm to design a Program
+ * using Classes and Objects.
+ *
+ * The Purpose of OOP is to simplify the Software Development and Maintenance
+ * by providing some concepts such as:
+ *      # Object
+ *      # Class
+ *      # Inheritance
+ *      # Polymorphism
+ *      # Abstraction
+ *      # Encapsulation
+ *
+ * Brief description about each concept:-
+ *
+ * Object:          It is an entity which has state and behavior.
+ *                  It can be physical or logical.
+ *                  Ex - Keyboard, Mouse, Pen, Screen etc.
+ *
+ * Class:           Collection of Objects is called Class.
+ *                  It is a logical entity.
+ *
+ * Inheritance:     When one object acquires all the properties and behaviours of parent object i.e. known as inheritance.
+ *                  It provides code re-usability. It is used to achieve runtime polymorphism.
+ *
+ * Polymorphism:    When one task is performed by different ways i.e. known as polymorphism.
+ *
+ * Abstraction:     Hiding internal details and showing functionality is known as abstraction.
+ *
+ * Encapsulation:   Binding (or wrapping) code and data together into a single unit is known as encapsulation.
+ */
+
+public class OOP0 {}
